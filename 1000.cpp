025efd4121f8cb69e206f2ad0865320a147e0112
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-01-13 16:02:20 +0900

    baekjoon: 1000
    백준 1000: A+B

---
+++ 1000.cpp
@@ -0,0 +1,21 @@
+/**
+ * Beakjoon 1000 c++ source code
+ * name : cwaa079
+ * date : 2021. 01. 13
+ * problem link : https://www.acmicpc.net/problem/1000
+
+ * Result
+   using memory : 808 KB
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A, B;
+
+	cin >> A >> B;
+	cout << A + B;
+
+	return 0;
+}
