aaee537022b3616bd734d997958557e1ff47dbcd
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-01-14 19:37:20 +0900

    baekjoon: 11022
    백준 11022번 문제 : A+B(출력형식)

---
+++ 11022.cpp
@@ -0,0 +1,24 @@
+/**
+* Baekjoon 11022 c++ source code
+* name : cwaa079
+* problem link : https://www.acmicpc.net/problem/11022
+
+* using memory : 1116 KB
+* time : 0ms
+*/
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		int num1, num2;
+		scanf("%d %d", &num1, &num2);
+		printf("Case #%d: %d + %d = %d\n", i + 1, num1, num2, num1 + num2);
+	}
+
+	return 0;
+}
