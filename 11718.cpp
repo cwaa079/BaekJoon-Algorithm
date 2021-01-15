0e05743a2586d2acfc91747a11d1e33fae361c60
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-01-15 19:12:20 +0900

    baekjoon: 11718
    백준 11718번 문제: 입력받은대로 출력

---
+++ 11718.cpp
@@ -0,0 +1,21 @@
+/**
+* Baekjoon 11718 c++ source code
+* name : cwaa079
+* problem link : https://www.acmicpc.net/problem/11718
+
+* using memory : 1116 KB
+* time : 0ms
+*/
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+	char str[100];
+	while (fgets(str, 100, stdin)) {
+		printf("%s", str);
+	}
+
+	return 0;
+}
