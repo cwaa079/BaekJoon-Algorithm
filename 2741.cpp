195efccdd3544da80cd97aa4737faa4b7b1c7b0c
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-01-15 19:15:32 +0900

    baekjoon: 2741
    백준 2741번 문제: 숫자 차례대로 출력

---
+++ 2741.cpp
@@ -0,0 +1,21 @@
+/**
+* Baekjoon 2741 c++ source code
+* name : cwaa079
+* problem link : https://www.acmicpc.net/problem/2741
+
+* using memory : 1116 KB
+* time : 12ms
+*/
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i < n + 1; i++) {
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
