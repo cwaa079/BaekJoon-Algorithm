74993825728ba99e50c341121bb76c8a0cc68da4
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-01-15 19:15:54 +0900

    baekjoon: 2742
    백준 2742번 문제: 내림차순 출력

---
+++ 2742.cpp
@@ -0,0 +1,22 @@
+/**
+* Baekjoon 2742 c++ source code
+* name : cwaa079
+* problem link : https://www.acmicpc.net/problem/2742
+
+* using memory : 1116 KB
+* time : 12ms
+*/
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--) {
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
