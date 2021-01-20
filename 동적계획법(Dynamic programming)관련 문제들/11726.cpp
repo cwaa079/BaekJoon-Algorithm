2dab91bdea709acadae63f77bdfd22d3050ab869
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-01-21 00:56:20 +0900

    baekjoon: 11726
    백준 11726번 문제: 2*n의 타일 채우기(피보나치수열)

---
+++ 동적계획법(Dynamic programming)관련 문제들/11726.cpp
@@ -0,0 +1,25 @@
+/**
+* Baekjoon 11726 c++ source code
+* name : cwaa079
+* problem link : https://www.acmicpc.net/problem/11726
+* using memory : 1116 KB
+* time : 0ms
+*/
+
+#include <stdio.h>
+#define MOD 10007
+
+int main()
+{
+	int n;
+	int dp[1001];
+	dp[0] = dp[1] = 1;
+
+	scanf("%d", &n);
+	for (int i = 2; i <= n; i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
+	}
+	printf("%d\n", dp[n]);
+
+	return 0;
+}
