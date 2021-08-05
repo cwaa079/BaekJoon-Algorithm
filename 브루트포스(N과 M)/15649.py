148b4ee3f84056d0ec0b8c5594a2a7631c1da80f
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-08-05 19:39:40 +0900

    Create 15649.py
    백트래킹, DFS

---
+++ 브루트포스(N과 M)/15649.py
@@ -0,0 +1,18 @@
+def solution(n, m):
+    s = []
+
+    def dfs():
+        if len(s) == m:
+            print(' '.join(map(str, s)))
+            return
+        for i in range(1, n + 1):
+            if i not in s:
+                s.append(i)
+                dfs()
+                s.pop()
+
+    return dfs()
+
+
+n, m = map(int, input().split())
+solution(n, m)
